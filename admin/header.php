ce99ae6e016eb9f3c96df83e39d8e87b7cbd9cb4
Bruno Gonçalves <bcgonca@gmail.com>
2020-10-08 22:07:29 -0300

    Fix: Header admin

--- admin/header.php
+++ admin/header.php
@@ -1,5 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	<a class="navbar-brand" href="#">Plataforma</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+	<a class="navbar-brand" href="/admin/">
+		<img src="/admin/img/logo.png" height="40" alt="Logo WoW Life">
+	</a>
 
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
     	<span class="navbar-toggler-icon"></span>
