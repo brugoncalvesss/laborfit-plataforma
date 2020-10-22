8b1544199d3fabae5196e6c6be4854edee5a962d
Bruno Gonçalves <bcgonca@gmail.com>
2020-10-22 20:28:36 -0300

    Fix: logut

--- admin/login/logout.php
+++ admin/login/logout.php
@@ -1,5 +1,6 @@
 <?php
-session_start();
-session_destroy();
+setcookie("LEMBRAR_USUARIO", false, time() - 1);
+unset($_COOKIE['USUARIO_EMPRESA']);
+unset($_COOKIE['USUARIO_NOME']);
 header("location: /admin/login/");
 exit();
