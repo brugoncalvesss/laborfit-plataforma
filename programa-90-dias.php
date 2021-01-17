8d9b4753de177f012c8cba2261e41b2d03ad12dd
Bruno Gonçalves <bcgonca@gmail.com>
2021-01-17 14:03:47 -0300

    Fix: Redirect programa

--- programa-90-dias.php
+++ programa-90-dias.php
@@ -1,62 +1,19 @@
 <?php
-require('control.php');
-require('_header.php');
+session_start();
+require_once './functions.php';
 
 $idPrograma = $_GET['programa'] ?: 1;
-$arPrograma = getPrograma($idPrograma);
-
-if (empty($_SESSION['USUARIO_ID'])) {
-    echo "Aconteceu algum erro, <a href='/logout.php'>Clique aqui</a> para entrar novamente.";
-    die();
-}
 
 $redirecionarParaPrograma = redirecionarParaPrograma($idPrograma, $_SESSION['USUARIO_ID']);
 if ($redirecionarParaPrograma) {
 	$url = "/programa.php?programa=".$idPrograma;
-	header('Location: '. $url);
+	header('Location: '. $url, true);
 	exit;
 }
 
+$url = "/introducao.php?programa=".$idPrograma;
+header('Location: '. $url, true);
+exit;
+
+require('_footer.php');
 ?>
-
-<main>
-	<nav class="navbar navbar-expand-md navbar-light bg-white">
-		<div class="container">
-
-			<a href="/" class="navbar-brand">
-				<img src="./img/logo.png" alt="Logo WoW Life" height="50">
-			</a>
-
-			<div class="dropdown">
-				<a class="dropdown-toggle" href="#" data-toggle="dropdown">
-					<img src="./img/user.png" alt="Perfil">
-				</a>
-				<div class="dropdown-menu dropdown-menu-right">
-					<a class="dropdown-item" href="/logout.php">Sair</a>
-				</div>
-			</div>
-		  
-		</div>
-	</nav>
-
-	<div class="bg-light py-4 mb-5">
-		<div class="container text-center">
-			<h2 class="h3 font-weight-bold mb-4"><?= $arPrograma[0]['NOME_PROGRAMA'] ?></h2>
-
-			<div class="offset-md-2 col-md-8">
-				<div class="embed-responsive embed-responsive-16by9">
-					<iframe class="embed-responsive-item" src="https://player.vimeo.com/video/<?= $arPrograma[0]['VIMEO_PROGRAMA'] ?>?title=0&byline=0&portrait=0&badge=0&showinfo=0&modestbranding=0" frameborder="0"></iframe>
-				</div>
-			</div>
-
-		</div>
-	</div>
-
-	<div class="container mb-5 text-center">
-		<p class="h4 mb-4">Participe do treinamento especial WOW Life 90 dias</p>
-		<p><a href="/programa.php?programa=<?= $arPrograma[0]['ID_PROGRAMA'] ?>" class="btn btn-dark">Começar</a></p>
-	</div>
-
-</main>
-
-<?php require('_footer.php'); ?>
